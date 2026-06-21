9bddc11dc7e5b2229920d82053cb060229a0191d
Muhammad Ahmad <theyamiprotocol@gmail.com>
2026-06-21 18:20:12 +0500

    Create index.ts

---
+++ src/control/index.ts
@@ -0,0 +1,6 @@
+export * from "./types.ts";
+export { classify } from "./classify.ts";
+export { runAction, runPlan, runTypedPlan, runComputerUse } from "./service.ts";
+export * from "./permissions.ts";
+export * as vision from "./vision.ts";
+export * as files from "./files.ts";
